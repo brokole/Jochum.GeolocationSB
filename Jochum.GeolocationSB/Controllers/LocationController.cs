7d468bc3bd241bfb58e5e1bc91abc70bf039b9d6
brokole <ajjkole@gmail.com>
2023-02-19 19:49:37 +0100

    catching uri

--- Jochum.GeolocationSB/Controllers/LocationController.cs
+++ Jochum.GeolocationSB/Controllers/LocationController.cs
@@ -179,29 +179,20 @@ namespace Jochum.GeoLocationsB.Controllers
             return Ok(Locations);
         }
 
-        [HttpGet("search ascending order sorting IdaswdfqD")]
         public static async void GetLocation()
         {
-            //Define your baseUrl
             string baseUrl = "https://api.positionstack.com/v1/forward";
-            //Have your using statements within a try/catch block
             try
             {
-                //We will now define your HttpClient with your first using statement which will use a IDisposable.
                 using (HttpClient client = new HttpClient())
                 {
-                    //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
-                    //The HttpResponseMessage which contains status code, and data from response.
                     using (HttpResponseMessage res = await client.GetAsync(baseUrl))
                     {
-                        //Then get the data or content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
                         using (HttpContent content = res.Content)
                         {
                             var data = await content.ReadAsStringAsync();
-                            //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
                             if (data != null)
                             {
-                                //Now log your data in the console
                                 Console.WriteLine("data------------{0}", data);
                             }
                             else
